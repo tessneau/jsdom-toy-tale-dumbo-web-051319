37f7bd06a84e025dc14eab39f78b5e04bf2562ad
L.R.L <DaleLoreNY@gmail.com>
2019-06-27 12:41:10 -0400

    Add Toy Info to the Card-DONE

--- src/index.js
+++ src/index.js
@@ -2,7 +2,36 @@ const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
 
+
+
+
 // YOUR CODE HERE
+
+function slapCardOnTheDOM(toyData){
+  toyData.forEach(function(toy){
+    const toyCollection = document.querySelector('#toy-collection')
+    const toyCard = document.createElement('div')
+    const h2Test = document.createElement('h2')
+    const p = document.createElement('p')
+    const image = `<img src="${toy.image}" class="toy-avatar" />`
+    const button = document.createElement('button')
+    // toyCard.setAttribute('class', 'card') same as line 18
+    toyCard.className="card"
+    button.className="like-btn"
+    toyCard.append(h2Test)
+    h2Test.innerHTML = (toy.name)
+    p.innerHTML = `${toy.likes} Likes`
+    button.innerText = ("Like <3")
+    toyCard.innerHTML += image
+    toyCard.append(p)
+    toyCard.append(button)
+    toyCollection.append(toyCard)
+
+  })
+}
+
+
+
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -17,3 +46,13 @@ addBtn.addEventListener('click', () => {
 
 
 // OR HERE!
+
+document.addEventListener("DOMContentLoaded", function(){
+  allToys()
+})
+
+function allToys(){
+  fetch("http://localhost:3000/toys")
+  .then(resp => resp.json())
+  .then(slapCardOnTheDOM)
+}
